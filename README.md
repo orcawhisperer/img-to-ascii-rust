176481a617e62e41a9506f55f620bb704c98c2b6
Vasantha Kumar <kvasanth373@gmail.com>
2023-12-29 18:44:38 +0530

    Image to Ascii rust/opencv

---
+++ README.md
@@ -0,0 +1,46 @@
+# img-to-ascii-rust
+
+This is a Rust program that converts an image into ASCII art.
+
+## Prerequisites
+
+- Rust (https://www.rust-lang.org/tools/install)
+- OpenCV (https://opencv.org/releases/)
+
+## Usage
+
+1. Clone the repository:
+
+    ```shell
+    git clone https://github.com/iamvasanth07/repo.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```shell
+    cd img-to-ascii-rust
+    ```
+
+3. Build the project:
+
+    ```shell
+    cargo build --release
+    ```
+
+4. Run the program with an image file as an argument:
+
+    ```shell
+    cargo run --release -- <image>
+    ```
+
+    Replace `<image>` with the path to your image file.
+
+5. The ASCII art will be saved to a text file with the same name as the image file, but with a `.txt` extension.
+
+## Example
+
+Suppose you have an image named 'example.jpg'. To convert it into ASCII art, you would run:
+
+    ```shell
+    cargo run --release -- example.jpg
+    ```
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use opencv::core;
+use opencv::imgcodecs;
+use opencv::imgproc;
+use opencv::prelude::*;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    // Parse command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <image>", args[0]);
+        return;
+    }
+
+    // Read image
+    let image = imgcodecs::imread(args[1].as_str(), imgcodecs::IMREAD_COLOR).unwrap();
+
+    // Convert to grayscale
+    let mut gray_image = Mat::default();
+    imgproc::cvt_color(&image, &mut gray_image, imgproc::COLOR_BGR2GRAY, 0).unwrap();
+
+    // Desired width or height
+    let desired_width = 150;
+    let desired_height = 150;
+
+    // Calculate scaling factor while maintaining aspect ratio
+    let scale_w = desired_width as f64 / image.cols() as f64;
+    let scale_h = desired_height as f64 / image.rows() as f64;
+    let scale = scale_w.min(scale_h);
+
+    let new_width = (image.cols() as f64 * scale) as i32;
+    let new_height = (image.rows() as f64 * scale) as i32;
+
+    // Resize image
+    let mut resized = Mat::default();
+    // let scale = 1.0;
+    imgproc::resize(
+        &gray_image,
+        &mut resized,
+        core::Size::new(new_width, new_height),
+        0.0,
+        0.0,
+        imgproc::INTER_LINEAR,
+    )
+    .unwrap();
+
+    // ASCII Converstion
+    let ascii_chars = "@%#*+=-:. "; // More chars for finer details
+    let mut ascii_art = String::new();
+
+    for i in 0..resized.rows() {
+        for j in 0..resized.cols() {
+            let pixel = resized.at_2d::<u8>(i, j).unwrap();
+            let ascii_index = *pixel as usize * ascii_chars.len() / 256;
+            ascii_art.push(ascii_chars.chars().nth(ascii_index).unwrap());
+        }
+        ascii_art.push('\n');
+    }
+
+    // Save ASCII art to file
+    let mut file = File::create(format!(
+        "{}.txt",
+        args[1].split('.').collect::<Vec<&str>>()[0]
+    ))
+    .unwrap();
+    file.write_all(ascii_art.as_bytes()).unwrap();
+
+    // Print ASCII art to console
+    println!("{}", ascii_art);
+
+    println!("Done!");
+}
